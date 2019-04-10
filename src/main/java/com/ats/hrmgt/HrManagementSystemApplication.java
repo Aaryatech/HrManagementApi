be6214f57a82946ecd8321536c9cf9c021024f52
ubuntu <ubuntu@ubuntu-Lenovo-ideapad-330-15IKB>
2019-04-10 16:09:36 +0530

    //initial comment

---
+++ src/main/java/com/ats/hrmgt/HrManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ats.hrmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagementSystemApplication.class, args);
+	}
+
+}
